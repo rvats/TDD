643ac024be81bf181fcc567a17e36981911f987c
rvats <vats.rahul@gmail.com>
2016-05-05 06:09:58 -0700

    tdd

---
+++ Filmstore.UnitTest/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Filmstore.UnitTest
+{
+    public class Class1
+    {
+    }
+}
---
+++ Filmstore.UnitTest/FilmTest.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Filmstore.UnitTest
+{
+    class FilmTest
+    {
+        [Test]
+        public void ConstructorShouldSetProperties()
+        {
+            //arrange
+            //act
+            Film film = new Film("Aliens", new DateTime(1984, 1, 20), 5 Genre.Science_Fiction);
+            //assert
+            Assert.AreEqual("Aliens", film.Title);
+            Assert.AreEqual(new DateTime(1984, 1, 20), film.Released);
+            Assert.AreEqual(5, film.Stock);
+            Assert.AreEqual(Genre.Science_Fiction, film.Genre);
+        }
+    }
+}
